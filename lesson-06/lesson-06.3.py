929e31151e0e6bf78af2eb4b4fb8534d8af70e4d
Tiunov Vladimir <tiunov2008.va@gmail.com>
2021-09-30 21:41:05 +0300

    fix: lesson-06

--- lesson-06/lesson-06.3.py
+++ lesson-06/lesson-06.3.py
@@ -3,7 +3,9 @@ mes2 = input()
 mes3 = input()
 if len(mes1) > len(mes2) and len(mes1) > len(mes3):
     print(mes1)
-elif len(mes2) > len(mes1) and len(mes2) > len(mes3):
+elif len(mes2) > len(mes3):
     print(mes2)
-elif len(mes3) > len(mes1) and len(mes3) > len(mes2):
+elif len(mes3) > len(mes2):
     print(mes3)
+elif len(mes1) == len(mes2) and len(mes1) == len(mes3):
+    print('Все статьи одинаковы по длине')
