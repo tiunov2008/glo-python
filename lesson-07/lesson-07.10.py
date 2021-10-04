26aacdd75197a9dd6c0be1ec40332b5dad33989e
Tiunov Vladimir <tiunov2008.va@gmail.com>
2021-10-04 19:20:16 +0300

    fix: lesson-07

--- lesson-07/lesson-07.10.py
+++ lesson-07/lesson-07.10.py
@@ -1,10 +1,7 @@
 n = int(input())
-flag = ''
+flag = 'YES'
 for i in range(n):
     num = int(input())
-    if num % 2 != 0:
-        if flag != 'NO':
-            flag = 'YES'
-    else:
+    if num % 2 == 0:
         flag = 'NO'
 print(flag)
