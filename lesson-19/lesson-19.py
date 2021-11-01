c3b3febba5ef4b1dd30d74a821853a988f616dbf
Tiunov Vladimir <tiunov2008.va@gmail.com>
2021-11-01 17:33:48 +0300

    lesson-19

---
+++ lesson-19/lesson-19.py
@@ -0,0 +1,69 @@
+import random
+#Функция для храннения данных
+def save(name, count_right_answers, result):
+    file = open('lesson-19/db.txt', 'a')
+    file.write(f'{name:20} {str(count_right_answers):25} {str(result):12}\n')
+    file.close()
+def get_results():
+    file = open('lesson-19/db.txt', 'r')
+    results = file.readlines()
+    print(f'{"Имя":20} Кол-во правильных ответов {"Результат":12}')
+    for i in results:
+        print(i, end='')
+    file.close()
+#Функция которая задает вопрос
+def ask_question(question):
+    print(question, ' Нажмите Да или Нет')
+    answer = input().lower().strip()
+    if answer == 'да':
+        return True
+    elif answer == 'нет':
+        return False
+    else:
+        print('Что-то пошло не так')
+        return ask_question(question)
+#Функция начала теста
+def start_test():
+    questions = [
+        'Сколько будет два делить на два умноженное на два?',
+        'Бревно нужно распилить на 10 частей, сколько надо сделать распилов?',
+        'На двух руках 10 пальцев. Сколько пальцев на 5 руках?',
+        'Укол делают каждые полчаса, сколько нужно минут для трех уколов?',
+        'Пять свечей горело две потухли. Сколько свечей осталось?',
+        'Сколько месяцев в году имеют 28 дней?',
+        'С какой скоростью должна двигаться собака (в возможных для неё пределах), чтобы не слышать звона сковородки, привязанной к ее хвосту?',
+        'У квадратного стола отпилили один угол по прямой линии . Сколько теперь углов у стола?'
+    ]
+    answers = [2, 9, 25, 60, 2, 12, 0, 5]
+    count_right_answers = 0
+    print('Как вас зовут?')
+    name = input()
+    for i in range(5):
+        question_index = random.randint(0,len(questions) - 1)
+        print('№', i + 1, questions[question_index])
+        user_answer = input()
+        while True:
+            if not user_answer.isdigit():
+                print('Пожалуйста, введите число!')
+                user_answer = input()
+            else:
+                user_answer = int(user_answer)
+                break
+        right_answer = answers[question_index]
+        if user_answer == right_answer:
+            count_right_answers += 1
+        questions.pop(question_index)
+        answers.pop(question_index)
+    print('Количество правильных ответов', name, '=', count_right_answers)
+    print(name, results[count_right_answers])
+    save(name, count_right_answers, results[count_right_answers])
+    print('Предыдущие результаты:')
+    get_results()
+    if ask_question('Хотите пройти тест еще раз?'):
+        start_test()
+
+
+
+results = ['Идиот','Кретин','Дурак','Нормальный','Талант','Гений']
+#Запуск теста
+start_test()
