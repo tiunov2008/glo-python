d8e75d8c384ed05c6a628013d907f47da0f4acd2
Tiunov Vladimir <tiunov2008.va@gmail.com>
2021-11-02 11:08:18 +0300

    fix

--- lesson-19/lesson-19.py
+++ lesson-19/lesson-19.py
@@ -1,16 +1,20 @@
 import random
+import math
 #Функция для храннения данных
-def save(name, count_right_answers, result):
+def save_db(name, count_right_answers, result):
     file = open('lesson-19/db.txt', 'a')
     file.write(f'{name:20} {str(count_right_answers):25} {str(result):12}\n')
     file.close()
-def get_results():
+def get_db():
     file = open('lesson-19/db.txt', 'r')
     results = file.readlines()
     print(f'{"Имя":20} Кол-во правильных ответов {"Результат":12}')
     for i in results:
         print(i, end='')
     file.close()
+def get_result(count_right_answers, len_questions):
+    results = ['Идиот','Кретин','Дурак','Нормальный','Талант','Гений']
+    return results[int(count_right_answers * (len(results) / len_questions))]
 #Функция которая задает вопрос
 def ask_question(question):
     print(question, ' Нажмите Да или Нет')
@@ -34,11 +38,12 @@ def start_test():
         'С какой скоростью должна двигаться собака (в возможных для неё пределах), чтобы не слышать звона сковородки, привязанной к ее хвосту?',
         'У квадратного стола отпилили один угол по прямой линии . Сколько теперь углов у стола?'
     ]
+    len_questions = len(questions)
     answers = [2, 9, 25, 60, 2, 12, 0, 5]
     count_right_answers = 0
     print('Как вас зовут?')
     name = input()
-    for i in range(5):
+    for i in range(len(questions)):
         question_index = random.randint(0,len(questions) - 1)
         print('№', i + 1, questions[question_index])
         user_answer = input()
@@ -55,15 +60,14 @@ def start_test():
         questions.pop(question_index)
         answers.pop(question_index)
     print('Количество правильных ответов', name, '=', count_right_answers)
-    print(name, results[count_right_answers])
-    save(name, count_right_answers, results[count_right_answers])
+    print(name, get_result(count_right_answers, len_questions))
+    save_db(name, count_right_answers, get_result(count_right_answers, len_questions))
     print('Предыдущие результаты:')
-    get_results()
+    get_db()
     if ask_question('Хотите пройти тест еще раз?'):
         start_test()
 
 
 
-results = ['Идиот','Кретин','Дурак','Нормальный','Талант','Гений']
 #Запуск теста
 start_test()
